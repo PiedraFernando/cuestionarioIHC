289f49af9d81ad273cd332e040364a56691a9f30
PiedraFernando <fernandopiedram@gmail.com>
2020-05-21 15:36:53 -0500

    html basico y pocos estilos

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuestionario IHC</title>
+    <link rel="shortcut icon" href="img/title_icon.png">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="bienvenida">
+            <h1>Cuestionario para IHC</h1>
+        </div>
+        <div class="preguntaContainer">
+            <div class="pregunta">
+                <span>Ingrese su nombre</span>
+            </div>
+            <input type="text" placeholder="Tu respuesta" require>
+        </div>
+        <div class="preguntaContainer">
+            <div class="pregunta">
+                <span>Ingrese su edad</span>
+                <span class="requerido">*</span>
+            </div>
+            <input type="text" placeholder="Tu respuesta">
+        </div>
+        <div class="preguntaContainer">
+            <div class="pregunta">
+                <span>Ingrese su sexo</span>
+                <span class="requerido">*</span>
+            </div>
+            <input type="text" placeholder="Tu respuesta">
+        </div>
+        <div class="preguntaContainer">
+            <div class="pregunta">
+                <span>grado de estudios</span>
+                <span class="requerido">*</span>
+            </div>
+            <input type="text" placeholder="Tu respuesta">
+        </div>
+        <div class="preguntaContainer">
+            <div class="pregunta">
+                <span>Como aprendio a utilizar la computadora</span>
+                <span class="requerido">*</span>
+            </div>
+            <input type="text" placeholder="Tu respuesta">
+        </div>
+        <input type="submit">
+    </div>
+</body>
+</html>
---
+++ page info/infoIconos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuestionario IHC</title>
+    <link rel="shortcut icon" href="img/title_icon.png">
+</head>
+<body>
+    <h1>Iconos usados</h1>
+    <div style="background-image: url('../img/title_icon.png');">
+        <a href="https://www.flaticon.es/autores/freepik" target="_blank" rel="icono del titulo">Icono de titulo</a>
+    </div>
+</body>
+</html>
